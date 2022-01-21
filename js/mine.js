ca1c7ef6067e33a69f26fb3f811044bd0f1dc96d
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2022-01-21 20:34:27 +0400

    obj

--- js/mine.js
+++ js/mine.js
@@ -69,3 +69,133 @@ let t = "5";
 let n = 5;
 
 console.log(Number(t) + n.toString())
+
+let global = "58";
+
+function calc(param1, param2, ...manyparam){
+    // local 
+    let localvir = "25";
+   //content
+    
+  // return Math.round(param1 * param2);
+  return manyparam;
+}
+
+// calc();
+
+  console.log(calc(5,5, "asdas", 12, "2e"))
+  console.log(calc(5,55))
+  console.log(calc(13,6))
+  console.log(calc(434.654,7))
+  console.log(calc(5,8))
+
+
+  let arrs = (myparam,sad) =>{
+      console.log(myparam)
+  }
+
+  arrs("arrow param");
+
+
+  let $ = el =>{
+      return document.getElementById(el);
+  }
+
+  $('fundiv').innerHTML = "ფუნქცია კარგი რამეა"
+
+
+  function ca(x, y){
+      //return (x * y).toFixed();
+     // return (x * y).toFixed(2);
+      // return (x * y).toFixed(3);
+  }
+
+console.log(ca(25.857, 65,7))
+
+function imgs(im){
+    document.getElementById('img').innerHTML += `<img src="${im}">`
+}
+
+imgs('img/bg.jpg');
+
+ 
+ 
+
+ function far(faren){
+     return (5/9) * (faren -32);
+ }
+
+ console.log(far(50) + "c");
+
+
+ let myglobal = 500;
+
+ mygloba();
+
+
+ function mygloba(){
+    myglobal = 200;
+ }
+
+
+
+
+ console.log(myglobal);
+
+ function changeText(msg){
+     let changeg = msg.replaceAll('HTML', '')
+     return changeg;
+ }
+
+ console.log(changeText('HTML არ არის პროგრამირების ენა'))
+
+ function cards(){
+     let cr = `<div class="card" style="width: 18rem;">
+     <img src="..." class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <a href="#" class="btn btn-primary">Go somewhere</a>
+     </div>
+   </div>`
+
+   document.getElementById('fundiv').innerHTML = cr;
+ }
+
+ cards();
+
+//  User Object
+
+let User = {
+    name:"ზურა",
+    email:"zura@gmail.com",
+    role:1,
+    price:25,
+    wieght:6.5545,
+    img:"img/logo.png",
+    active:true,
+    changepw:true,
+    usename: (objname)=>{
+     return ( this.price *  this.wieght).toFixed() + "₾"; 
+    }
+}
+
+Object.freeze(User); // ობიექტის გაყინვა (ცვლილებებს აღარ ასახავს)
+
+User.changepw = false; // ობიექტის თვისების შეცვლა
+delete User.role; // ობიექტის თვისების წაშლა 
+User.mynames = "bla vla"; // ობიექტის თვისების ჩამატება
+
+User.usename();
+
+User["active"];
+User.active;
+
+let key = Object.keys(User);// აბრუნებს ობიექტის  ყველა თვისებას მასივის სახით
+let value = Object.values(User);// აბრუნებს ობიექტის  ყველა მნიშვნელობას მასივის სახით
+let allval = Object.entries(User);// აბრუნებს ობიექტის    ყველა თვისებას და მათ მნიშვნელობას
+console.log(allval) 
+
+for (const keys in User) {
+   console.log(typeof User[keys]);
+}
